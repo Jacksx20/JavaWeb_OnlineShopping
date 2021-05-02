9d19d7e0086d9f1b5f685d38d1ca7f2b90ba0a40
Jack20 <chunjcsx20@vip.qq.com>
2021-05-02 15:03:02 +0800

    type.java

---
+++ Shopping/src/java/model/Type.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Jacksx20
+ * 
+ */
+public class Type {
+    private int code;
+    private String title;
+    
+    public Type() {}
+
+    public Type(int code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
