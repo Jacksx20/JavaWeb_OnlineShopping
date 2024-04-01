d6ae4cd1c54a157483d7991dc29e2cedfd956f34
Jack20 <chunjcsx20@vip.qq.com>
2024-04-01 14:43:09 +0800

    fix

--- Shopping/src/java/controller/ModifyAmountServlet.java
+++ Shopping/src/java/controller/ModifyAmountServlet.java
@@ -6,7 +6,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
