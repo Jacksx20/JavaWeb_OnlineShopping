e4ae3f08b7370fcb6ecb9d056f3cda62292f1a61
Jack20 <chunjcsx20@vip.qq.com>
2024-04-09 11:10:21 +0800

    Create QueryServlet.java
    Co-Authored-By: sgyt <83530976+sgyt2020@users.noreply.github.com>

---
+++ Shopping/src/java/controller/QueryServlet.java
@@ -0,0 +1,57 @@
+
+package controller;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import model.DBUtils;
+
+@MultipartConfig
+@WebServlet(name = "QueryServlet", urlPatterns = {"/QueryServlet"})
+public class QueryServlet extends HttpServlet {
+//订单查询
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        String fm="<tr><th><input type='checkbox'></th><td><img src='images/%s.jpg' width='60'></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%.2f</td><td>%d</td></tr>",//网页的一部分
+               tb="";
+        try(    Connection cn=DBUtils.connect();//建立联系
+                PreparedStatement ps=cn.prepareStatement("select * from wares where type="+request.getParameter("type"));//预处理SQL语句
+                ResultSet rs=ps.executeQuery()) {//上传SQL语句进行查询
+            while(rs.next()) {
+                tb+=String.format(fm, rs.getString("photo"),//构建tb
+                        rs.getString("code"),
+                        rs.getString("title"),
+                        rs.getString("model"),
+                        rs.getString("depict"),
+                        rs.getDouble("price"),
+                        rs.getInt("amount"));
+            }
+        }catch(SQLException se) {
+            se.printStackTrace();
+        }
+        try(PrintWriter out=response.getWriter()) {
+            out.print(tb);//输出tb
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try(    Connection cn=DBUtils.connect();
+                PreparedStatement ps=cn.prepareStatement("delete wares where code in ("+ request.getParameter("cds")+")")){//预处理SQL语句
+            ps.executeUpdate();//上传SQL语句进行查询
+        }catch(SQLException se){
+            se.printStackTrace();
+        }
+    }
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
