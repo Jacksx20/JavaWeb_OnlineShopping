093f091968747d9346a2df35cd1d285efa7d30bf
Jack20 <chunjcsx20@vip.qq.com>
2024-04-09 11:06:24 +0800

    Add Order.java

---
+++ Shopping/src/java/model/Order.java
@@ -0,0 +1,78 @@
+
+package model;
+
+import java.io.Serializable;
+
+
+public class Order {
+    private long code;
+    private String owner, date, consignee, phone, address;
+    private double total;
+    public Order(){}
+    public Order(long code, String owner, String date, String consignee, String phone, String address, double total){
+        this.code = code;
+        this.owner = owner;
+        this.address = address;
+        this.date = date;
+        this.total = total;
+        this.phone = phone;
+        this.consignee = consignee;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getConsignee() {
+        return consignee;
+    }
+
+    public void setConsignee(String consignee) {
+        this.consignee = consignee;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+}
